611ff302a3f25aea1a71eba9f5e21706d418bc9e
Sergio David Fernández Villa <37827861+SergioDavidFernandezVilla@users.noreply.github.com>
2024-12-19 19:46:17 -0900

    Add files via upload

---
+++ PractLomok/src/main/java/com/example/PractLomok/PractLomokApplication.java
@@ -0,0 +1,13 @@
+package com.example.PractLomok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractLomokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractLomokApplication.class, args);
+	}
+
+}
---
+++ PractLomok/src/test/java/com/example/PractLomok/PractLomokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PractLomok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractLomokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
